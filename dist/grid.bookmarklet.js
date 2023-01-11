ef24404d5b8dc31ad0f9ae2ab0ca0268f78d40d6
Marc Görtz <mail@marcgoertz.de>
2023-01-11 20:40:17 +0100

    Refactor grid bookmarklet.

--- dist/grid.bookmarklet.js
+++ dist/grid.bookmarklet.js
@@ -1,1 +1,65 @@
-(()=>{var o="grid-bookmarklet",t=document.body,A=document.querySelector(`#${o}`),e=document.createElement("div"),s="position:fixed;top:0;left:0;z-index:1;width:100%;height:100%;user-select:none;pointer-events:none;background-position:0 0;";(A==null?void 0:A.className)==="plain"?A.remove():((A==null?void 0:A.className)==="rows"?(A.remove(),e.className="plain",e.style.cssText=`${s}background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAAAQBAMAAABzZ+XyAAAAD1BMVEX///8zzMzMmcwzzMyKr8xbQuVpAAAABXRSTlMARER3dw+R52kAAAAnSURBVChTY3BxMQYCQSDAR7u4MIwqxKtQSQnEYQACfLSS0qhCvAoBfpk/wSqLG9QAAAAASUVORK5CYII=)`):(e.className="rows",e.style.cssText=`${s}background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABCBAMAAAAyIUwOAAAAFVBMVEX///8zzMzMmcyZmZkzzMyKr8x2qqoSHzPEAAAAB3RSTlMARERVd3d3b6zWQAAAAEZJREFUSEtjDGV4z8DAcBaIjfHQggyMowrxKlRicCFC4R6QQvsNDGBTBRlw0qw8owpHFY4qHFU4qnBU4ajCUYXDQCFxDSQAonZUrjWIoNsAAAAASUVORK5CYII=)`),e.id=o,t.append(e));})();
+(()=>{var e="grid-bookmarklet",r=document.querySelector(`#${e}`),n=`
+:root {
+  --${e}-offset: 0;
+  --${e}-max-width: 76rem;
+  --${e}-columns: 6;
+  --${e}-gutter: 1rem;
+  --${e}-color: hsla(204, 80%, 72%, 0.25);
+}
+
+@media (min-width: 36rem) {
+  :root {
+		--${e}-offset: 1rem;
+    --${e}-color: hsla(286, 51%, 44%, 0.25);
+  }
+}
+
+@media (min-width: 48rem) {
+  :root {
+		--${e}-offset: 0;
+    --${e}-columns: 12;
+		--${e}-max-width: 44rem;
+    --${e}-color: hsla(204, 80%, 72%, 0.25);
+  }
+}
+
+@media (min-width: 62rem) {
+  :root {
+		--${e}-max-width: 60rem;
+    --${e}-color: hsla(286, 51%, 44%, 0.25);
+  }
+}
+
+@media (min-width: 80rem) {
+  :root {
+		--${e}-max-width: 76rem;
+    --${e}-color: hsla(204, 80%, 72%, 0.25);
+  }
+}
+
+:root {
+  --${e}-repeating-width: calc(100% / var(--${e}-columns));
+  --${e}-column-width: calc((100% / var(--${e}-columns)) - var(--${e}-gutter));
+  --${e}-background-width: calc(100% + var(--${e}-gutter));
+  --${e}-background: repeating-linear-gradient(
+    to right,
+    var(--${e}-color),
+    var(--${e}-color) var(--${e}-column-width),
+    transparent var(--${e}-column-width),
+    transparent var(--${e}-repeating-width)
+  );
+}
+
+#${e} {
+  position: fixed;
+  top: 0; right: 0; bottom: 0; left: 0;
+  z-index: 1;
+  margin: auto;
+  width: calc(100% - (2 * var(--${e}-offset)));
+  max-width: var(--${e}-max-width);
+  background-image: var(--${e}-background);
+  background-size: var(--${e}-background-width) 100%;
+  pointer-events: none;
+  user-select: none;
+}
+`,a;if(r)r.remove(),(a=document.querySelector(`#${e}-styles`))==null||a.remove();else{let t=document.createElement("style");t.textContent=n,t.id=`${e}-styles`,document.head.append(t);let o=document.createElement("div");o.id=e,document.body.append(o)}})();
--- src/grid.ts
+++ src/grid.ts
@@ -1,25 +1,82 @@
 const id = 'grid-bookmarklet';
-const body = document.body;
 const node = document.querySelector(`#${id}`);
-const grid = document.createElement('div');
 
-const css
-	= 'position:fixed;top:0;left:0;z-index:1;width:100%;height:100%;user-select:none;pointer-events:none;background-position:0 0;';
+const css = `
+:root {
+  --${id}-offset: 0;
+  --${id}-max-width: 76rem;
+  --${id}-columns: 6;
+  --${id}-gutter: 1rem;
+  --${id}-color: hsla(204, 80%, 72%, 0.25);
+}
 
-if (node?.className === 'plain') {
+@media (min-width: 36rem) {
+  :root {
+		--${id}-offset: 1rem;
+    --${id}-color: hsla(286, 51%, 44%, 0.25);
+  }
+}
+
+@media (min-width: 48rem) {
+  :root {
+		--${id}-offset: 0;
+    --${id}-columns: 12;
+		--${id}-max-width: 44rem;
+    --${id}-color: hsla(204, 80%, 72%, 0.25);
+  }
+}
+
+@media (min-width: 62rem) {
+  :root {
+		--${id}-max-width: 60rem;
+    --${id}-color: hsla(286, 51%, 44%, 0.25);
+  }
+}
+
+@media (min-width: 80rem) {
+  :root {
+		--${id}-max-width: 76rem;
+    --${id}-color: hsla(204, 80%, 72%, 0.25);
+  }
+}
+
+:root {
+  --${id}-repeating-width: calc(100% / var(--${id}-columns));
+  --${id}-column-width: calc((100% / var(--${id}-columns)) - var(--${id}-gutter));
+  --${id}-background-width: calc(100% + var(--${id}-gutter));
+  --${id}-background: repeating-linear-gradient(
+    to right,
+    var(--${id}-color),
+    var(--${id}-color) var(--${id}-column-width),
+    transparent var(--${id}-column-width),
+    transparent var(--${id}-repeating-width)
+  );
+}
+
+#${id} {
+  position: fixed;
+  top: 0; right: 0; bottom: 0; left: 0;
+  z-index: 1;
+  margin: auto;
+  width: calc(100% - (2 * var(--${id}-offset)));
+  max-width: var(--${id}-max-width);
+  background-image: var(--${id}-background);
+  background-size: var(--${id}-background-width) 100%;
+  pointer-events: none;
+  user-select: none;
+}
+`;
+
+if (node) {
 	node.remove();
+	document.querySelector(`#${id}-styles`)?.remove();
 } else {
-	if (node?.className === 'rows') {
-		node.remove();
-		grid.className = 'plain';
-		grid.style.cssText
-			= `${css}background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAAAQBAMAAABzZ+XyAAAAD1BMVEX///8zzMzMmcwzzMyKr8xbQuVpAAAABXRSTlMARER3dw+R52kAAAAnSURBVChTY3BxMQYCQSDAR7u4MIwqxKtQSQnEYQACfLSS0qhCvAoBfpk/wSqLG9QAAAAASUVORK5CYII=)`;
-	} else {
-		grid.className = 'rows';
-		grid.style.cssText
-			= `${css}background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABCBAMAAAAyIUwOAAAAFVBMVEX///8zzMzMmcyZmZkzzMyKr8x2qqoSHzPEAAAAB3RSTlMARERVd3d3b6zWQAAAAEZJREFUSEtjDGV4z8DAcBaIjfHQggyMowrxKlRicCFC4R6QQvsNDGBTBRlw0qw8owpHFY4qHFU4qnBU4ajCUYXDQCFxDSQAonZUrjWIoNsAAAAASUVORK5CYII=)`;
-	}
+	const style = document.createElement('style');
+	style.textContent = css;
+	style.id = `${id}-styles`;
+	document.head.append(style);
 
+	const grid = document.createElement('div');
 	grid.id = id;
-	body.append(grid);
+	document.body.append(grid);
 }
